6243cbd0b9c4db1af49dfc72ec5cfc73c19bc279
TealGlow <alyssacomstock95@gmail.com>
2021-10-16 19:26:28 -0700

    Initial Commit.

---
+++ HelloWorld.c
@@ -0,0 +1,16 @@
+//Name: Alyssa Comstock
+//Date: 10/16/2021
+//Description: Simple Hello World Program!  For the start of my C practice!
+
+
+#include <stdio.h>
+
+
+
+
+int main(){
+    // simple program that outputs hello world
+    printf("Hello World!");
+
+    return 0;
+}
